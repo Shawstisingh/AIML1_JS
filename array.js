e9d946992b801f9b92fef8f562c1ad57afd961e4
Shawsti Singh <shawstisingh>
2026-03-09 15:51:31 +0530

    add

---
+++ array.js
@@ -0,0 +1,22 @@
+var myinfo = new Array();
+var myinfo = Array();
+console.log(myinfo, myinfo1);
+
+
+let collection ={
+    {};
+    [];
+    true,
+    "jhon",
+    function () {};
+    90009898193,
+    undefined.
+    null,
+    new String("abc"),
+    new Date(),
+}
+console.log(collection);
+collection.teacherName ="Jhon Mac";
+collection.phoneNo = 950008193;
+
+//dom = document object model
