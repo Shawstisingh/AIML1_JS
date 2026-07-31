6123451b1ec37793993085387041073f63f38c13
Shawsti Singh <shawstisingh>
2026-07-31 11:07:32 +0530

    Push latest changes

--- day 1/day 2/A.js
+++ day 1/day 2/A.js
@@ -1,4 +1,15 @@
-var firstName = "Shawsti";
-var lastName = "Singh";
-console.log(firstName);
-console.log(lastName);
+// var firstName = "Shawsti";
+// var lastName = "Singh";
+// console.log(firstName);
+// console.log(lastName);
+
+var a=40;
+var b=30;
+if(a>b)
+{
+    console.log("A is greater, value of A is: ", a);
+}
+else
+{
+    console.log("B is greater, value of B is: ", b);
+}
