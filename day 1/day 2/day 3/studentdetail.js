e88bdb3fcd102f1f5f72ec0495ff455089842439
Shawsti Singh <shawstisingh>
2026-08-14 11:08:23 +0530

    Update studentdetail.js

--- day 1/day 2/day 3/studentdetail.js
+++ day 1/day 2/day 3/studentdetail.js
@@ -1,12 +1,24 @@
 //student details
-console.log("-----STUDENT DETAIL-----");
-var student={
-    name: "Shawsti Singh",
-    id: 182,
-    branch: "computer science",
-    department: "AIML",
-};
-console.log("Student Name: " + student.name);
-console.log("Student ID: " + student.id);
-console.log("Student Branch: " + student.branch);
-console.log("Student Department: " + student.department);
+// console.log("-----STUDENT DETAIL-----");
+// var student={
+//     name: "Shawsti Singh",
+//     id: 182,
+//     branch: "computer science",
+//     department: "AIML",
+// };
+// console.log("Student Name: " + student.name);
+// console.log("Student ID: " + student.id);
+// console.log("Student Branch: " + student.branch);
+// console.log("Student Department: " + student.department);
+
+
+//even odd using if else
+    var a= 20;
+    if(a%2==0)
+    {
+        console.log("The number is even");
+    }
+    else
+    {
+        console.log("The number is odd");
+    }
