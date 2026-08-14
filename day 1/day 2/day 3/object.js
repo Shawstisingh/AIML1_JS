07477b135dc1b416a01a1aa080aadad7871e0085
Shawsti Singh <shawstisingh>
2026-08-14 11:41:27 +0530

    Update object.js

--- day 1/day 2/day 3/object.js
+++ day 1/day 2/day 3/object.js
@@ -1,10 +1,21 @@
-console.log("===== object methods =====");
-const student = {
-    name: "Shawsti Singh",
-    id: 182,
-    course: "B.Tech",
-    branch: "Computer Science",
-    department: "AIML",
-};
-console.log(student);
-console.log("Student Name: " + student.name);
+// 
+
+console.log("=====array of objects=====");
+    const users = [
+        {
+            id: 1,
+            name: "Shawsti Singh",
+        },
+        {
+            id: 2,
+            name: "Rimjhim Keshri",
+        },
+        { 
+            id: 3,
+            name: "Sakshi Pandey",
+        },
+    ];
+        console.log(users[0].name);
+        console.log(users[1].name);
+        console.log(users[2].name);
+
