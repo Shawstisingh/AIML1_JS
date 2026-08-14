b56ebc72cd9a38b207a14de9a0e5a046c8dae754
Shawsti Singh <shawstisingh>
2026-08-14 11:05:44 +0530

    Add day 3 student detail files

---
+++ day 1/day 2/day 3/studentdetail.js
@@ -0,0 +1,12 @@
+//student details
+console.log("-----STUDENT DETAIL-----");
+var student={
+    name: "Shawsti Singh",
+    id: 182,
+    branch: "computer science",
+    department: "AIML",
+};
+console.log("Student Name: " + student.name);
+console.log("Student ID: " + student.id);
+console.log("Student Branch: " + student.branch);
+console.log("Student Department: " + student.department);
