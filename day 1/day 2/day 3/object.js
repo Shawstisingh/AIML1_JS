63d1d32cb4ed8ee09c57e87ffc6f3be36e37edec
Shawsti Singh <shawstisingh>
2026-08-14 11:35:22 +0530

    Add object.js

---
+++ day 1/day 2/day 3/object.js
@@ -0,0 +1,9 @@
+console.log("===== object methods =====");
+const student = {
+    name: "Shawsti Singh",
+    id: 182,
+    course: "B.Tech",
+    branch: "Computer Science",
+    department: "AIML",
+};
+console.log(student);
