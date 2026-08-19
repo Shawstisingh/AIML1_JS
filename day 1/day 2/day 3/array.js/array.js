7f111b7f09f5b7b0eda93e3b59e536abaf4d1ca2
Shawsti Singh <shawstisingh>
2026-08-19 13:24:36 +0530

    Add string methods example

---
+++ day 1/day 2/day 3/array.js/array.js
@@ -0,0 +1,9 @@
+console.log("=====array of objects=====");
+let name = "Shawsti Singh";
+console.log(name);
+//string length
+console.log("length=" + name.length);
+//1. toUpperCase()
+console.log("Uppercase=" + name.toUpperCase());
+//2. toLowerCase()
+console.log("Lowercase=" + name.toLowerCase());
