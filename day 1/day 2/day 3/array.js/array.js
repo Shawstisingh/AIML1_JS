56dc403f88a6cf27875c089997db2a2e4b4bd8f5
Shawsti Singh <shawstisingh>
2026-08-19 13:49:40 +0530

    Update array JavaScript example

--- day 1/day 2/day 3/array.js/array.js
+++ day 1/day 2/day 3/array.js/array.js
@@ -1,38 +1,50 @@
-console.log("=====array of objects=====");
-let name = "Shawsti Singh";
-console.log(name);
-//string length
-console.log("length=" + name.length);
-//1. toUpperCase()
-console.log("Uppercase=" + name.toUpperCase());
-//2. toLowerCase()
-console.log("Lowercase=" + name.toLowerCase());
-//3. charAt()
-console.log("charAt=" + name.charAt(0));
-//4. indexOf()
-console.log("indexOf=" + name.indexOf("S"));
-//5. lastIndexOf()
-let message ="JavaScript is a programming language.";
-console.log("lastIndexOf=" + message.lastIndexOf("is"));
-//6. slice()
-console.log(
-    "slice=" ,
-    message.slice(0, 7)
-);
-//7.split()
-let data="html,css,javascript,react";
-let result=data.split(",");
-console.log("split=" ,result);
-//8. replace()
-let text="I love JavaScript";
-let newText=text.replace("love","like");
-console.log("replace=" ,newText);
+// console.log("=====string=====");
+// let name = "Shawsti Singh";
+// console.log(name);
+// //string length
+// console.log("length=" + name.length);
+// //1. toUpperCase()
+// console.log("Uppercase=" + name.toUpperCase());
+// //2. toLowerCase()
+// console.log("Lowercase=" + name.toLowerCase());
+// //3. charAt()
+// console.log("charAt=" + name.charAt(0));
+// //4. indexOf()
+// console.log("indexOf=" + name.indexOf("S"));
+// //5. lastIndexOf()
+// let message ="JavaScript is a programming language.";
+// console.log("lastIndexOf=" + message.lastIndexOf("is"));
+// //6. slice()
+// console.log(
+//     "slice=" ,
+//     message.slice(0, 7)
+// );
+// //7.split()
+// let data="html,css,javascript,react";
+// let result=data.split(",");
+// console.log("split=" ,result);
+// //8. replace()
+// let text="I love JavaScript";
+// let newText=text.replace("love","like");
+// console.log("replace=" ,newText);
 
-//9.concat()
-let first="shawsti";
-let last="singh";
-let fullName=first.concat(" ",last);
-console.log("concat=" ,fullName);
-//10.includes()
-let includesText=message.includes("JavaScript");
-console.log("includes=" ,includesText);
+// //9.concat()
+// let first="shawsti";
+// let last="singh";
+// let fullName=first.concat(" ",last);
+// console.log("concat=" ,fullName);
+// //10.includes()
+// let includesText=message.includes("JavaScript");
+// console.log("includes=" ,includesText);
+  
+
+
+console.log("=====array of objects=====");  
+let student=[
+    {
+        id:1,
+        name:"Shawsti Singh",
+        age:20
+    }
+];
+console.log(student);
