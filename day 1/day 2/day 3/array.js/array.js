9f0fe29b96ee746a0e5d83049d0675d9055ade06
Shawsti Singh <shawstisingh>
2026-08-19 13:40:24 +0530

    Update array JavaScript example

--- day 1/day 2/day 3/array.js/array.js
+++ day 1/day 2/day 3/array.js/array.js
@@ -7,3 +7,23 @@ console.log("length=" + name.length);
 console.log("Uppercase=" + name.toUpperCase());
 //2. toLowerCase()
 console.log("Lowercase=" + name.toLowerCase());
+//3. charAt()
+console.log("charAt=" + name.charAt(0));
+//4. indexOf()
+console.log("indexOf=" + name.indexOf("S"));
+//5. lastIndexOf()
+let message ="JavaScript is a programming language.";
+console.log("lastIndexOf=" + message.lastIndexOf("is"));
+//6. slice()
+console.log(
+    "slice=" ,
+    message.slice(0, 7)
+);
+//7.split()
+let data="html,css,javascript,react";
+let result=data.split(",");
+console.log("split=" ,result);
+//8. replace()
+let text="I love JavaScript";
+let newText=text.replace("love","like");
+console.log("replace=" ,newText);
--- day 1/day 2/day 3/object.js
+++ day 1/day 2/day 3/object.js
@@ -1,21 +1,40 @@
 // 
 
-console.log("=====array of objects=====");
-    const users = [
-        {
-            id: 1,
-            name: "Shawsti Singh",
-        },
-        {
-            id: 2,
-            name: "Rimjhim Keshri",
-        },
-        { 
-            id: 3,
-            name: "Sakshi Pandey",
-        },
-    ];
-        console.log(users[0].name);
-        console.log(users[1].name);
-        console.log(users[2].name);
+// console.log("=====array of objects=====");
+//     const users = [
+//         {
+//             id: 1,
+//             name: "Shawsti Singh",  //obj 1
+//         },
+//         {
+//             id: 2,
+//             name: "Rimjhim Keshri",   //obj 2
+//         },
+//         { 
+//             id: 3,
+//             name: "Sakshi Pandey",   //obj 3 
+//         },
+//     ];
+//         console.log(users[0].name);
+//         console.log(users[1].name);
+//         console.log(users[2].name);
+
+// const employee = {
+//     id:182,
+//     name: "Shawsti Singh",
+//     department: "AIML",
+
+//     displayDetails: function()
+//     {
+//         console.log("Employee ID: " + this.id);
+//         console.log("Employee Name: " + this.name);
+//         console.log("Employee Department: " + this.department);
+//     }
+    
+
+//adding property
+studentData.course="B.Tech";
+console.log(studentData)
+//delete property
+delete studentData.age;
 
