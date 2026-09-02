0513197cc518d92f13b682f1859593793375ca3f
Shawsti Singh <shawstisingh>
2026-09-02 14:29:31 +0530

    Fix object.js syntax and add function calls

--- day 1/day 2/day 3/array.js/object.js
+++ day 1/day 2/day 3/array.js/object.js
@@ -1,26 +1,15 @@
-// let c={
-//     age:20,
-//     job:"teacher",
-//     details:function(){
-//         console.log("my age is:"+this.age+" and my job is: "+this.job);
-// },
-// profile:"assistant professor",
-// job_title:function(){
-//     console.log("my job title is: "+this.profile);
-// }
-// }
-
-const a={
-    name:"john",
-    age:25,
-    salary:50000,
-    display:function(){
-        {
-            console.log("my name is: "+this.name+" and my age is: "+this.age+" and my salary is: "+this.salary);
-        },
-        increasesalary:function(){
-            this.salary+=10000;
-            console.log("my salary is: "+this.salary);
-        }
-
+const a = {
+    name: "john",
+    age: 25,
+    salary: 50000,
+    display: function() {
+        console.log("my name is: " + this.name + " and my age is: " + this.age + " and my salary is: " + this.salary);
+    },
+    increasesalary: function() {
+        this.salary += 10000;
+        console.log("my salary is: " + this.salary);
+    }
 }
+
+a.display(); 
+a.increasesalary();
