e061b3e94a76e27bfe2e43185312af73a250ce27
Shawsti Singh <shawstisingh>
2026-09-02 14:35:40 +0530

    Fix object.js with proper object syntax and show function

--- day 1/day 2/day 3/array.js/object.js
+++ day 1/day 2/day 3/array.js/object.js
@@ -1,15 +1,38 @@
-const a = {
-    name: "john",
-    age: 25,
-    salary: 50000,
-    display: function() {
-        console.log("my name is: " + this.name + " and my age is: " + this.age + " and my salary is: " + this.salary);
-    },
-    increasesalary: function() {
-        this.salary += 10000;
-        console.log("my salary is: " + this.salary);
-    }
+// const a = {
+//     name: "john",
+//     age: 25,
+//     salary: 50000,
+//     display: function() {
+//         console.log("my name is: " + this.name + " and my age is: " + this.age + " and my salary is: " + this.salary);
+//     },
+//     increasesalary: function() {
+//         this.salary += 10000;
+//         console.log("my salary is: " + this.salary);
+//     }
+// }
+
+// a.display(); 
+// a.increasesalary();
+
+
+
+
+function show() {
+    console.log("My address is: " + this.address);
+    console.log("My city is: " + this.city);
 }
 
-a.display(); 
-a.increasesalary();
+const e = {
+    address: "123 Main St",
+    city: "New York",
+    show: show
+};
+
+const f = {
+    address: "456 Elm St",
+    city: "Los Angeles",
+    show: show
+};
+
+e.show();
+f.show();
