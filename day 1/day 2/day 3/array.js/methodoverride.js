483cd935f982de582b16e4de85d04e442344ca03
Shawsti Singh <shawstisingh>
2026-09-21 15:40:39 +0530

    Fix method override salary example

---
+++ day 1/day 2/day 3/array.js/methodoverride.js
@@ -0,0 +1,23 @@
+class employee{
+    constructor(id, name, basicSalary){
+        this.id = id;
+        this.name = name;
+        this.basicSalary = basicSalary;
+    }
+    calculateSalary(){
+        return this.basicSalary;
+}
+}
+class manager extends employee{
+    constructor(id, name, basicSalary, incentive){
+        super(id, name, basicSalary);
+        this.incentive = incentive;
+    }
+    calculateSalary(){
+        return super.calculateSalary() + this.incentive;
+    }
+}
+let l1 = new employee(182, "Shawsti", 50000);
+console.log("Employee Salary:", l1.calculateSalary());      
+let m1 = new manager(154, "Rumjhum", 60000, 10000);
+console.log("Manager Salary:", m1.calculateSalary());
