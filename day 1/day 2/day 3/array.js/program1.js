baff4d57325fa83eeab2857061657db0410f3f27
Shawsti Singh <shawstisingh>
2026-09-25 11:59:53 +0530

    Add Node.js file system examples

--- day 1/day 2/day 3/array.js/program1.js
+++ day 1/day 2/day 3/array.js/program1.js
@@ -1,22 +1,72 @@
-const EventEmitter = require('events');
-const ud=new EventEmitter();
-ud.on('greet', (name) => {
-    console.log(`Hello there 2B, ${name}`)
-});
-ud.on('exit', (num) => {
-    console.log(`thank you for visiting ${num}`)
-});
-ud.emit('greet', 'Shawsti');
-ud.emit('exit', 182 );
+// const EventEmitter = require('events');
+// const ud=new EventEmitter();
+// ud.on('greet', (name) => {
+//     console.log(`Hello there 2B, ${name}`)
+// });
+// ud.on('exit', (num) => {
+//     console.log(`thank you for visiting ${num}`)
+// });
+// ud.emit('greet', 'Shawsti');
+// ud.emit('exit', 182 );
 
 
- class Button extends EventEmitter{
-    click(){
-        console.log("button clicked");
-        this.emit('click' , { timestamp: Date.now() });
+//  class Button extends EventEmitter{
+//     click(){
+//         console.log("button clicked");
+//         this.emit('click' , { timestamp: Date.now() });
+//     }
+// };
+// const button = new Button();
+// button.on('click', (event) => {
+//     console.log(`button clicked at ${event.timestamp}`);
+// });
+// button.click();
+
+
+
+// console.log("start")
+// setTimeout(() => {
+//     console.log("inside settimeout");
+// }, 2000);
+// setImmediate(() => {
+//     console.log("inside setImmediate");
+// });
+// process.nextTick(() => {
+//     console.log("inside nextTick");
+// });
+// console.log("end")
+
+const fs = require('fs');
+fs.writeFile("std.txt", "name: Shawsti",(err)=>{
+    if(err)
+    {   
+        console.log("error occured",err);
     }
-};
-const button = new Button();
-button.on('click', (event) => {
-    console.log(`button clicked at ${event.timestamp}`);
+    else
+    {
+        console.log("file created successfully");
+    }
+})
+
+fs.readFile("std.txt", "utf-8",(err,data)=>{
+    if(err)
+    {
+        console.log("error occured",err);
+    }
+    else
+    {
+        console.log("file read successfully",data);
+    }
+})
+
+fs.appendFile("std.txt", 
+    "rollno: 182",(err)=>{
+    if(err)
+    {   
+        console.log("error occured",err);
+    }
+    else
+    {
+        console.log("file appended successfully");
+    }
 })
